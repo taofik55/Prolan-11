977f010c361df1d48fd5032ce5cd252fd59a8cab
Taofik Rakhman Sudrajat <taofikrakhmansudrajat@gmail.com>
2017-02-28 13:17:15 +0700

    Add files via upload

---
+++ Pertemuan 2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa{
+public static void main(String[] args) {
+  System.out.println("Nama   : Taofik Rakhman Sudrajat");
+  System.out.println("NIM    : 10115155");
+  System.out.println("Alamat : Sumedang");
+}
+}
