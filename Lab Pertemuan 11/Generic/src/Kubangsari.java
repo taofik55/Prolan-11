909174c2bc204ce791ee69ac0424ac0aee4c2805
Taofik Rakhman Sudrajat <taofikrakhmansudrajat@gmail.com>
2017-05-31 19:53:16 +0700

    Add files via upload

---
+++ Lab Pertemuan 11/Generic/src/Kubangsari.java
@@ -0,0 +1,11 @@
+public class Kubangsari<T> {
+    private T tipe;
+    
+    public T getTipe (){
+        return tipe;
+    }
+    
+    public void setTipe (T tipe){
+        this.tipe = tipe;
+    }
+}
